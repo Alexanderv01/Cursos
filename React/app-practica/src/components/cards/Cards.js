1ce7c861c74b83ec91873a6773748f478a29c19d
Alexanderv01 <86639841+Alexanderv01@users.noreply.github.com>
2021-07-13 21:50:27 -0500

    Update Cards.js
    nuevas actualizacion

--- React/app-practica/src/components/cards/Cards.js
+++ React/app-practica/src/components/cards/Cards.js
@@ -1,21 +1,22 @@
 
-//import foto from '../../images/Fotos1.jpg'
-
-
+import { Button } from 'bootstrap'
+import React, {useEffect, useState} from 'react'
 const Cards = ({user}) => {
-    const {name,login,picture}= user
+    const {name,login,picture} = user
+    const [SEGUIR, setColor] = useState(true)
+
     console.log(user)
     return (
-        
-        <div className="card" >
-            <img src={picture.large} className="card-img-top" alt={login.username}/>
-             <div className="card-body">
-                <h5 className="card-title">{name.last}</h5>
-                <a href="#" className="btn btn-primary">Seguir</a>
-            </div>
-        </div>   
+        <div className = "card" >
+                <img src={picture.large} className="card-img-top" alt={login.username}/>
+                    <div className="card-body">                        
+                    <h5 className="card-title">{name.first}</h5>
+                    <buttom
+                         href="#" className={SEGUIR ? "btn btn-danger": "btn btn-dark" }
+                        onClick={()=>setColor (!SEGUIR)}>Seguir</buttom>
+                    </div>
+        </div>
     )
 }
 
 export default Cards
-
